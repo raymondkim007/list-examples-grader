627d2b94d5a877aea87924fe9ea01f14fb9eb3b1
Raymond Kim :D <raymondkim123456789@gmail.com>
2023-06-08 20:17:43 -0700

    update grade.sh

--- grade.sh
+++ grade.sh
@@ -5,12 +5,48 @@ rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
-echo 'Finished cloning'
+git clone $1 student-submission 2> /dev/null
+echo 'Submission Cloned'
 
+# check submission for correct file
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+student_file=`find student-submission -name ListExamples.java`
+if ! [[ -f $student_file ]]
+then 
+    echo "Student submission not found"
+    exit
+else 
+    echo "Student submission found. Path: $student_file"
+fi
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+# copy all relevant files to grading-area
+cp $student_file TestListExamples.java grading-area/
+
+# compile Java files
+cd grading-area
+javac -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" *.java
+if [[ $? -eq 1 ]]
+then 
+    echo "ERROR: Java file cannot be compiled. Exit code 1"
+    exit
+fi
+
+# run JUnit tests
+echo "Running JUnit tests..."
+java -cp ".;../lib/junit-4.13.2.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > grade-results.txt
+grep "Tests run" grade-results.txt > fail-results.txt
+grep "OK" grade-results.txt > succ-results.txt
+
+failcheck=`wc fail-results.txt`
+
+echo "Tests completed. Results shown below:"
+if [[ $failcheck == "0 0 0 fail-results.txt" ]]
+then 
+    echo "All tests passed!"
+else 
+    failmsg=$(head -n 1 fail-results.txt)
+    testcnt=${failmsg:11:1}
+    failcnt=${failmsg:25:2}
+    succcnt=$(($testcnt - $failcnt))
+    echo "Score: $succcnt / $testcnt"
+fi
